c8a68bf18506c801d3aa2fc5c6b55c8fe20c221a
zamirulkabir <zamirulkabir999@gmail.com>
2023-03-31 16:43:55 +0600

    mark as read click display update on bookmarked blogs

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import Cards from "../Cards/Cards";
-import logo from "../Image/card2.jpg";
 
 const Blogs = () => {
   const [cards, setCards] = useState([]);
+  const [cardInfo, setCardInfo] = useState([]);
 
   useEffect(() => {
     fetch("fakeDB.json")
@@ -11,34 +11,39 @@ const Blogs = () => {
       .then((data) => setCards(data));
   }, []);
 
+  const handelMarkAsRead = (card) => {
+    const newCardInfo = [...cardInfo, card];
+    setCardInfo(newCardInfo);
+  };
+
   return (
-    
-      <div className="container grid lg:grid-cols-4  lg:gap-4 lg:px-10 py-10">
-        <div className="lg:col-span-3 border-2 p-4">
-          {cards.map((card) => (
-            <Cards  card={card} 
+    <div className="container grid lg:grid-cols-4  lg:gap-4 lg:px-10 py-10">
+      <div className="lg:col-span-3 border-2 p-4">
+        {cards.map((card) => (
+          <Cards
+            card={card}
             key={card.id}
-            ></Cards>
-          ))}
-        </div>
-
-
-        <div className=" p-4 sm:mt-2">
-          <div className="">
-            <h2 className="lg:text-lg font-bold mb-4">
-              Spent time on read : 177 min
-            </h2>
-          </div>
-          <div className=" ">
-            <h2 className="lg:text-lg font-bold">Bookmarked Blogs : 8</h2>
-            <div className="mt-6 bg-slate-50 p-2 rounded-lg">
-              <p>Master Microsoft Power Platform and Become an In-Demand!</p>
-            </div>
-          </div>
-        </div>
-
+            handelMarkAsRead={handelMarkAsRead}
+          ></Cards>
+        ))}
       </div>
-  
+
+      <div className=" p-4 sm:mt-2">
+        <div className="">
+          <h2 className="lg:text-lg font-bold mb-4">
+            Spent time on read : 177 min
+          </h2>
+        </div>
+        <div className=" ">
+          <h2 className="lg:text-lg font-bold">
+            Bookmarked Blogs : {cardInfo.length}
+          </h2>
+          <div className="mt-6 bg-slate-50 p-2 rounded-lg">
+            <p>Master Microsoft Power Platform and Become an In-Demand!</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,24 +1,37 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 
 const Cards = (props) => {
   const { img, name, date, time, title, parsonImg } = props.card;
+  const handelMarkAsRead = props.handelMarkAsRead;
+
   return (
     <div>
-      <div >
-        <div >
+      <div>
+        <div className="mb-4">
           <img className=" h-96 w-full" src={img} alt="" />
-          <div className="flex justify-between">
-            <div className="flex gap-4">
+          <div className="flex justify-between mt-4 ">
+            <div className="flex gap-4 ">
               <img className="h-12 w-12 rounded-full" src={parsonImg} alt="" />
               <div>
                 <p>{name}</p>
                 <p>{date}</p>
               </div>
             </div>
-            <div>{time} min read</div>
+            <div>{time} min read  <FontAwesomeIcon className="cursor-pointer " icon={faBookmark} /></div>
           </div>
-          <p>{title}</p>
-          <p>Mark as read</p>
+          <p className="text-xl font-bold">{title}</p>
+          <div className="cursor-pointer ">
+            <button
+              className="text-blue-500 mb-4 mt-4"
+              onClick={() => handelMarkAsRead(props.card)}
+            >
+              Mark as read
+             
+            </button>
+          </div>
+          <div className="border-b-2"></div>
         </div>
       </div>
     </div>
